a16da33b1442e5fc6cb76d984bbaa912d3eb1d8f
agates4 <agate97@gmail.com>
2018-10-06 15:07:34 -0400

    new commit

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000
+var app = express.createServer();
 
-app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+app.set("view options", {layout: false});
+app.use(express.static(__dirname + '/views'));
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-  response.render('index.html');
-})
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
-
+app.listen(8080, '127.0.0.1')
