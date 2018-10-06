0b40c59c792811b0512a77756198926afa8064a2
agates4 <agate97@gmail.com>
2018-10-06 16:21:20 -0400

    added console log for this one

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.post('/help1', (req, res) => {
+    console.log(req)
+})
+
 app.listen(80, () => {
     // console.log('http://localhost:5656')
 })
