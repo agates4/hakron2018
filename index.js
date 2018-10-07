077018d7f8e3333ce8b24f5bb71eb9853a4b6d7c
agates4 <agate97@gmail.com>
2018-10-07 02:32:09 -0400

    final update to the weather

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.post('/weather', (req, res) => {
     weather.setForecastType("daily");
     weather.setCulture('en')
 
+    formattedResponse = ""
+
     weather.now("Akron", function(err, data) {	
         if(err) console.log(err);
         else {
@@ -41,25 +43,51 @@ app.post('/weather', (req, res) => {
             sunset.setHours(sunset.getHours() - 4);
             var formattedSunset = dateFormat(sunset, "h:MM TT");
 
-            console.log(formattedSunrise, formattedSunset, data.values.weather, data)
+            formattedResponse = "The sun will rise at " + formattedSunrise
+            + " and set at " + formattedSunset + ". "
         }
+
+        request.post({
+            headers: {'content-type' : 'application/x-www-form-urlencoded'},
+            url:     'http://api.openweathermap.org/data/2.5/forecast?q=Akron&units=imperial&appid=272962de1269f77934c7e7c37e4a915c&cnt=20',
+            body:    "mes=heydude"
+        }, function(error, response, body){
+            weatherResponse = JSON.parse(body)
+    
+            currentDay = false
+            insertDay = false
+            for (let index = 0; index < weatherResponse.cnt; index++) {
+                const element = weatherResponse.list[index];
+
+                date = element.dt * 1000
+                var date = new Date(date)
+                date.setHours(date.getHours() - 4);
+
+                compareDay = dateFormat(date, "mmm dd");
+                if(currentDay != compareDay) {
+                    currentDay = dateFormat(date, "mmm dd");
+                    insertDay = true
+                } else insertDay = false
+
+                insertString = ""
+                if(insertDay)
+                    insertString = "On " + currentDay + ":\n"
+
+                var formattedDate = dateFormat(date, "h:MM TT");
+                formattedResponse += insertString + "\n" + "There will be " + element.weather[0].description + " at " + formattedDate
+            }
+
+            res.send(
+                {
+                    "response": formattedResponse
+                }
+            )
+        });
     })
 
-    request.post({
-        headers: {'content-type' : 'application/x-www-form-urlencoded'},
-        url:     'http://localhost/test2.php',
-        body:    "mes=heydude"
-    }, function(error, response, body){
-        console.log(body);
-    });
-
-    // http://api.openweathermap.org/data/2.5/forecast?q=Akron&units=imperial&appid=272962de1269f77934c7e7c37e4a915c&cnt=20
     
-    res.send(
-        {
-            "response": "weather"
-        }
-    )
+
+    
 })
 app.post('/addiction', (req, res) => {
     const placeName = ["ad1", "ad2", "ad3"]
