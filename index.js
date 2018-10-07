5a03981654b79d9d9d9836bb8d54faa36b1a9f6f
Matt McCombs <mmccomb6@kent.edu>
2018-10-07 12:26:21 -0400

    fixed merge conflits

--- index.js
+++ index.js
@@ -22,14 +22,9 @@ app.post('/bus', (req, res) => {
     res.send(
         {
             "response": 
-<<<<<<< HEAD
-            bus [0] + " " + closestTerminal [0] + " " + arrival[0] + " " + "\n" 
-    
-=======
             bus [0] + " " + closestTerminal [0] + " " + arrival[0] + " " + "\n"
         }
     )
->>>>>>> fb11284ca082ed307eb1c6a202ab09696155fbbb
 })
 
 app.post('/weather', (req, res) => {
@@ -86,32 +81,6 @@ app.post('/weather', (req, res) => {
                 var formattedDate = dateFormat(date, "h:MM TT");
                 formattedResponse += insertString + "\n" + "There will be " + element.weather[0].description + " at " + formattedDate
             }
-<<<<<<< HEAD
-            })
-
-            res.send({
-            "response": "weather"
-            })
-            })
-            app.post('/addiction', (req, res) => {
-                
-                const placeName = ["#1 Salvation Army,", "#2 Summa Rehab,", "#3 IBH Addiction Recovery Center,"]
-                const hours = ["HOURS: 9am - 7pm,", "Open all day,", "HOURS: 8am - 6pm,"]
-                const address = ["1006 Grant St, Akron, OH,", "29 N Adams St, Akron, OH,", "3445 S Main St, Akron, OH,"]
-                const distance = ["1.5 miles from current location", "2.2 miles from current location", "5.3 miles from current location"]
-
-                console.log(req.body)
-                res.send({
-                    "response": placeName[0] + " " + hours[0] + " " + address[0] + " " + distance[0] + "\n" +
-                        placeName[1] + " " + hours[1] + " " + address[1] + " " + distance[1] + "\n" +
-                        placeName[2] + " " + hours[2] + " " + address[2] + " " + distance[2] + "\n"
-                })
-            })
-
-            app.listen(80, () => {
-                // console.log('http://localhost:5656')
-            })
-=======
 
             res.send(
                 {
@@ -148,4 +117,3 @@ app.post('/addiction', (req, res) => {
 app.listen(80, () => {
     // console.log('http://localhost:5656')
 })
->>>>>>> fb11284ca082ed307eb1c6a202ab09696155fbbb
