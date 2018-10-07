0cfb23f4f526dd6c030ed4596e7c7b4d5202891c
agates4 <agate97@gmail.com>
2018-10-07 12:21:38 -0400

    fixing some error

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/bus', (req, res) => {
     res.send(
         {
             "response": 
-            bus [0] + " " + closestTerminal [0] + " " + arrival[0] + " " + "\n" +
+            bus [0] + " " + closestTerminal [0] + " " + arrival[0] + " " + "\n"
         }
     )
 })
@@ -46,37 +46,37 @@ app.post('/weather', (req, res) => {
             sunset.setHours(sunset.getHours() - 4);
             var formattedSunset = dateFormat(sunset, "h:MM TT");
 
-            console.log(formattedSunrise, formattedSunset, data.values.weather, data)
-            }
-            })
+        }
+    })
 
-            res.send({
-            "response": "weather"
-            })
-            })
-            app.post('/addiction', (req, res) => {
-                request.get({
-                    headers: {'zipCode' : 'application/x-www-form-urlencoded'},
-                    url:     'https://www.zipcodeapi.com/rest/GOhazMBKVJ2VDSEOrrkf0sswW4D5c4NYOjZi2mGTjf2wuvgvTkUj5L1KpR2GkRRI/info.json/zip_code/degrees'
-                }, function(error, response, body){
-                    data = JSON.parse(body)
+    res.send({
+    "response": "weather"
+    })
+})
 
-                    console.log(data)
-                })
-                
-                const placeName = ["#1 Salvation Army,", "#2 Summa Rehab,", "#3 IBH Addiction Recovery Center,"]
-                const hours = ["HOURS: 9am - 7pm,", "Open all day,", "HOURS: 8am - 6pm,"]
-                const address = ["1006 Grant St, Akron, OH,", "29 N Adams St, Akron, OH,", "3445 S Main St, Akron, OH,"]
-                const distance = ["1.5 miles from current location", "2.2 miles from current location", "5.3 miles from current location"]
+app.post('/addiction', (req, res) => {
+    request.get({
+        headers: {'zipCode' : 'application/x-www-form-urlencoded'},
+        url:     'https://www.zipcodeapi.com/rest/GOhazMBKVJ2VDSEOrrkf0sswW4D5c4NYOjZi2mGTjf2wuvgvTkUj5L1KpR2GkRRI/info.json/zip_code/degrees'
+    }, function(error, response, body){
+        data = JSON.parse(body)
 
-                console.log(req.body)
-                res.send({
-                    "response": placeName[0] + " " + hours[0] + " " + address[0] + " " + distance[0] + "\n" +
-                        placeName[1] + " " + hours[1] + " " + address[1] + " " + distance[1] + "\n" +
-                        placeName[2] + " " + hours[2] + " " + address[2] + " " + distance[2] + "\n"
-                })
-            })
+        console.log(data)
+    })
+    
+    const placeName = ["#1 Salvation Army,", "#2 Summa Rehab,", "#3 IBH Addiction Recovery Center,"]
+    const hours = ["HOURS: 9am - 7pm,", "Open all day,", "HOURS: 8am - 6pm,"]
+    const address = ["1006 Grant St, Akron, OH,", "29 N Adams St, Akron, OH,", "3445 S Main St, Akron, OH,"]
+    const distance = ["1.5 miles from Akron University Law Building", "2.2 miles from Akron University Law Building", "5.3 miles from Akron University Law Building"]
 
-            app.listen(80, () => {
-                // console.log('http://localhost:5656')
-            })
+    console.log(req.body)
+    res.send({
+        "response": placeName[0] + " " + hours[0] + " " + address[0] + " " + distance[0] + "\n" +
+            placeName[1] + " " + hours[1] + " " + address[1] + " " + distance[1] + "\n" +
+            placeName[2] + " " + hours[2] + " " + address[2] + " " + distance[2] + "\n"
+    })
+})
+
+app.listen(80, () => {
+    // console.log('http://localhost:5656')
+})
